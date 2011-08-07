ed04e3e05099898520f2607bd93cba2da5704edd
VI <iam@visualidiot.com>
2011-08-07 17:50:59 +0100

    *  Added a callback function *  Rewrote it, so it's on multiple lines. *  Added support for multiple scripts in one call.

--- teleport.js
+++ teleport.js
@@ -1,1 +1,31 @@
-function teleport(script){var head = document.getElementsByTagName('head')[0];script_tag = document.createElement('script');script_tag.src = script;script_tag.type = 'text/javascript';head.appendChild(script_tag)}function include(script){teleport(script)}
+/*
+ *      Teleport.JS
+ *      Originally by @hunterhdolan, changes by @visualidiot.
+ */
+ 
+function teleport(what, callback) {
+
+    //  Check if it's an array…
+    if(what.toString().indexOf('Array') == -1) {
+        for(i = 0; i < what.length; i++) {
+            import(what[i]);
+        }
+    } else {
+        import(what);
+    }
+
+    //  Function-ception.
+    function import(script) {
+        var head = document.getElementsByTagName('head')[0];
+            script_tag = document.createElement('script');
+            script_tag.src = script;
+            script_tag.type = 'text/javascript';
+        
+            head.appendChild(script_tag)    
+    }
+    
+    //  And run a callback with the function.
+    if(typeof callback == 'function') {
+        callback();
+    }
+}
